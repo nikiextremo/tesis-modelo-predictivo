e1ab22e7e15e4a5271df995927eee20b4ec6f8e8
Niki Pazmino <niki.pazmino@cu.ucsg.edu.ec>
2023-07-03 23:42:23 -0500

    primeros cambios

---
+++ resources/js/components/Index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import  ReactDOM from "react-dom";
+
+function Index() {
+    return <h1> Index/sa </h1>
+}
+
+export default Index;
+
+ReactDOM.render(<Index />, document.getElementById('index'));
