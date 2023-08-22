c3a831741b27ca9db2ed2711617c6efb3acfc5e6
Niki Pazmino <niki.pazmino@cu.ucsg.edu.ec>
2023-08-21 20:30:54 -0500

    providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,18 +12,33 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        //
-    }
+    // public function register()
+    // {
+    //     //
+    // }
 
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
+    // public function boot()
+    // {
+    //     //
+    // }
+
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https://');
+        }
+    }
+
+
+    public function register()
+    {
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
